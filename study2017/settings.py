44bc973ff31b294504ad0e3476847bf456c00174
fido2478 <fido2478@gmail.com>
2017-03-18 10:22:20 +0900

    added sqlite

--- study2017/settings.py
+++ study2017/settings.py
@@ -77,6 +77,10 @@ WSGI_APPLICATION = 'study2017.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
+    'postgresql': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'study2017',
         'USER': 'jio',
